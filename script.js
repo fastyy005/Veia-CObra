dcdd7ce5c32cc4a54215df1f4cacafaafd929875
fastyy005 <110356678+fastyy005@users.noreply.github.com>
2025-12-29 14:54:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function mostrarVideo() {
   const telaVideo = document.getElementById("videoTela");
   telaVideo.classList.remove("hidden");
   
-  // Procura o vídeo dentro da tela e dá o play
   const video = telaVideo.querySelector("video");
   if (video) {
     video.play().catch(error => console.log("Erro ao dar play:", error));
