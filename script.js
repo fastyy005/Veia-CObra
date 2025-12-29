c0e8f08e1a61b50357957a3f3f1e5c741dd8752b
fastyy005 <110356678+fastyy005@users.noreply.github.com>
2025-12-29 14:24:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarPergunta() {
+  document.getElementById("inicio").classList.add("hidden");
+  document.getElementById("pergunta").classList.remove("hidden");
+}
+
+function mostrarVideo() {
+  document.getElementById("pergunta").classList.add("hidden");
+  document.getElementById("videoTela").classList.remove("hidden");
+}
